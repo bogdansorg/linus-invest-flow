fbc24d6e423c3743f6f231ce46564ac647860538
bciobo <b.ciobotaru@solvemate.com>
2020-10-30 19:23:04 +0100

    create list view
    * new model for Project data
    * new components: ListItem and Step
    * stored the api JSON response in a file just in case

---
+++ frontend/src/components/ListItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Project } from "../models";
+
+export const ListItem: React.FC<Project> = ({id, name, location }) => {
+  return (
+    <li key={id} style={{'margin': '0 0 5px 0'}}>
+        <div className='box'>
+          <h1 className="title">{name}</h1>
+          <h2 className="subtitle">{location}</h2>
+        </div>
+    </li>
+  );
+};
