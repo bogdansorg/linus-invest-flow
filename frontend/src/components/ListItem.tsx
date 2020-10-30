04702607ee34b38b1d31b6708c4326a04afe0d5e
bciobo <b.ciobotaru@solvemate.com>
2020-10-30 19:49:55 +0100

    make list item background light grey

--- frontend/src/components/ListItem.tsx
+++ frontend/src/components/ListItem.tsx
@@ -4,7 +4,7 @@ import { Project } from "../models";
 export const ListItem: React.FC<Project> = ({id, name, location }) => {
   return (
     <li key={id} style={{'margin': '0 0 5px 0'}}>
-        <div className='box'>
+        <div className='box has-background-light'>
           <h1 className="title">{name}</h1>
           <h2 className="subtitle">{location}</h2>
         </div>
