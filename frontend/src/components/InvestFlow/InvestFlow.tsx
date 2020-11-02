a2cb58976cc632e108ead3c7ffc6b6b60c4df5b6
bciobo <b.ciobotaru@solvemate.com>
2020-11-02 20:26:00 +0100

    add sub-component for first step `SelectProjectStep`

--- frontend/src/components/InvestFlow/InvestFlow.tsx
+++ frontend/src/components/InvestFlow/InvestFlow.tsx
@@ -1,56 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { Project } from "../../models";
-import * as jsonResponse from "../../api_response.json";
-import { ListItem } from "../ListItem";
-import { StepLayout } from "../StepLayout";
+import { SelectProjectStep } from "./SelectProjectStep/SelectProjectStep";
 
 
 export const InvestFlow: React.FC = () => {
-  const [projects, setProjects] = useState<Project[]>([] as Project[]);
-  const [status, setStatus] = useState('idle');
+  const [stepIndex, setStep] = useState(1);
 
-  useEffect(() => {
-    setStatus('loading');
-    const getProjects = async () => {
-      const url = `https://fullstack.linus-capital.com/projects`;
-      try {
-        const response = await fetch(url, {
-          headers: {
-            'Accept': '*/*'
-          },
-        });
-        const data = await response.json();
-        setProjects(data.projects as Project[]);
-      } catch (e) {
-        console.error(e);
-        setProjects(jsonResponse.projects as Project[]);
-      } finally {
-        setStatus('resolved');
-      }
-    };
-    getProjects();
-  }, [])
+  let currentStep;
+  switch (stepIndex) {
+    case 1:
+      currentStep = <SelectProjectStep/>
+      break;
 
-  const listElements = projects.map((project) => <ListItem key={project.id}
-                                                           project={project}
-                                                           onSelect={(id) => {
-                                                             console.log('Selected project ID:', id)
-                                                           }}/>);
+    default:
+      currentStep = <SelectProjectStep/>
+      break;
+  }
   return (
-    <StepLayout number={1}
-                subtitle={'Select the project you want to invest in'}>
-      {status === 'loading' && (
-        <button className={'button is-loading is-large is-fullwidth'}
-                style={{'border': 'none'}}>
-        </button>
-      )
-      }
-      {status === 'resolved' && (
-        <ul>
-          {listElements}
-        </ul>
-      )
-      }
-    </StepLayout>
-  );
+    <>
+      {currentStep}
+    </>
+  )
 }
