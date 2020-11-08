b22953407a032b748d4b6e4d3e3acd8e5ae0c422
bciobo <b.ciobotaru@solvemate.com>
2020-11-08 21:12:45 +0100

    test case where the data cannot be saved due to an error

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -10,13 +10,22 @@ client = TestClient(app)
 
 
 class InvestResourceTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.new_investment = {'email': 'bogdan@mail.com', 'amount': 200000, 'project_id': 33}
 
     @patch('backend.app.InvestmentService', spec=InvestmentService)
     def test_create_investment(self, investmentServiceMock):
-        investment_data = {'email': 'bogdan@mail.com', 'amount': 200000, 'project_id': 33}
         new_id = 100
         inv_serv = investmentServiceMock.return_value
         inv_serv.save.return_value = new_id
-        response = client.post("/investments/", json=investment_data)
+        response = client.post("/investments/", json=self.new_investment)
         assert response.status_code == 200
         assert response.json() == {"id": new_id}
+
+    @patch('backend.app.InvestmentService', spec=InvestmentService)
+    def test_create_exception_handled(self, investmentServiceMock):
+        inv_serv = investmentServiceMock.return_value
+        inv_serv.save.side_effect = Exception('error')
+        response = client.post("/investments/", json=self.new_investment)
+        assert response.status_code == 503
+        assert response.json() == {"detail": 'Invest service is down. Please try again later.'}
